3ffad32c6bdf9f532e1a62da11f77b052c995321
Anat Abdullagasim <anat.pantera@gmail.com>
2020-04-20 23:45:36 +0700

    Initial project

---
+++ src/main/java/com/anat/aws/SpringBootAwsReactApplication.java
@@ -0,0 +1,13 @@
+package com.anat.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsReactApplication.class, args);
+	}
+
+}
