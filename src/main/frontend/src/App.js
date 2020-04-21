52829ddb370e894506fad48df9806e1f9c659119
Anat Abdullagasim <anat.pantera@gmail.com>
2020-04-21 10:50:04 +0700

    Modified display user profile

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -4,9 +4,12 @@ import './App.css';
 import axios from "axios"
 
 const UserProfile = () => {
+  const [userProfiles, setUserProfiles] = useState([]);
+
   const fetchUserProfiles = () => {
     axios.get("http://localhost:8080/api/v1/user-profile").then(res => {
       console.log(res);
+      setUserProfiles(res.data);
     })
   };
 
@@ -14,7 +17,14 @@ const UserProfile = () => {
     fetchUserProfiles();
   }, [])
 
-  return <h1>Hello</h1>
+  return userProfiles.map((userProfile, index) => {
+    return (
+      <div key={index}>
+        <h1>{userProfile.username}</h1>
+        <p>{userProfile.uuid}</p>
+      </div>
+    )
+  });
 }
 
 function App() {
